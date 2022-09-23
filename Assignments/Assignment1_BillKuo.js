600816a9d7a47fa14d4e9fd9fe144eb4d0a66ba4
Bill Kuo <billkuo08@gmail.com>
2022-09-22 22:37:21 -0700

    Q1~5

--- Assignments/Assignment1_BillKuo.js
+++ Assignments/Assignment1_BillKuo.js
@@ -1,9 +1,65 @@
 // 1. Write a function to find the sum of the elements inside the array
+console.log("Q1");
+
+const MyArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+getSum = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+};
+console.log(getSum(MyArr));
+
 // 2. Using a function for loop print all even numbers up to n (parameter in function);
+console.log("\nQ2");
+
+allEvenNums = (n) => {
+    for (let j = 0; j <= n; j++) {
+        if (j % 2 === 0) {
+            console.log(j);
+        };
+
+    };
+};
+allEvenNums(10);
+
+
 // 3. Write a function take a string as a parameter, Check if a string contains the letter “y”. If yes print "YES" and only one time.
-    // For example - "Crayzy" "Yes"
+// For example - "Crayzy" "Yes"
+console.log("\nQ3");
+hasYandy = (x) => {
+    let str = JSON.stringify(x);
+    let lowY = str.includes("y");
+    let upY = str.includes("Y");
+    if (lowY || upY) {
+        return "YES";
+    } else {
+        return "Parameter must be a string"
+    };
+
+};
+console.log(hasYandy("Crazy"));
+console.log(hasYandy("York"));
+console.log(hasYandy(0));
+
 
 // 4. Write a function to find the factorial of a given number n
+console.log("\nQ4");
+findFactorial = (n) => {
+    let answer = 1;
+    if (n == 0 || n == 1) {
+        return answer;
+    } else {
+        for (let i = n; i >= 1; i--) {
+            answer = answer * i;
+        }
+        return answer;
+    }
+
+}
+console.log(`The factorial of the given number is ${findFactorial(5)}`);
+
 
 // 5. // Write a function which accepts 4 parameters (different scores of a student for subjects), and
 // then calculate the average of all the score.
@@ -11,6 +67,28 @@
 // If the average is between 70 and 90 , then console grade B
 // If the average is between 50-70, then console grade C
 // Other wise console grade F
+console.log("\nQ5");
+reportCard = (subj1 = 0, subj2 = 0, subj3 = 0, subj4 = 0) => {
+    let subjects = [subj1, subj2, subj3, subj4];
+    let total = 0;
+    var average = total / subjects.length;
+    for (let score in subjects) {
+        total += subjects[score];
+    }
+    if (average >= 90) {
+        return "A";
+    } else if (average >= 70) {
+        return "B";
+    } else if (average >= 50) {
+        return "C";
+    } else {
+        return "F";
+    }
+
+}
+
+console.log(`The grade of the student is ${reportCard(90, 70, 50, 49)}`);
+
 
 // 6. Write a function which prints the star pattern like that for the given value n
 
