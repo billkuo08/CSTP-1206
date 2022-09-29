7e7e1017fe7f3197eb78ec8f43dcb6f4a92eda3e
Bill Kuo <billkuo08@gmail.com>
2022-09-29 14:12:11 -0700

    updated

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -4,22 +4,34 @@ function submitForm() {
 
     let data = {
         name: "",
-        email: ""
+        email: "",
+        studentID:""
     }
 
     // Saving new form submit data
-    userList.push(data);
+     userList.push(data);
+
+    // data1 = document.querySelector('#name').value ;
+    // userList.push(data1);
+    // data2 = document.querySelector('#email').value;
+    // userList.push(data2);
+    // data3= document.querySelector('#ID').value;
+    // userList.push(data3);
+
+
 
     localStorage.setItem("userList", JSON.stringify(userList));
-}
 
+}
 
 // You can get the userlist using
 
 let updatedUserList = JSON.parse(localStorage.getItem("userList"));
 
-for (let i = 0 ; i < updatedUserList.length ; i++) {
-    
-    document.getElementById('table').textContent = updatedUserList;
-    
+console.log(updatedUserList)
+
+for (let i = 0; i < updatedUserList.length; i++) {
+
+    document.getElementById.apply('#x').innerHTML = "bojour";    
+
 }
