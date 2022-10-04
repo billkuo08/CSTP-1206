56620f66254f6823cc4caf176b1ca68b9279abd5
Bill Kuo <billkuo08@gmail.com>
2022-10-04 11:42:24 -0700

    picture excercise

--- jsonpackage/index.js
+++ jsonpackage/index.js
@@ -1,17 +1,28 @@
 // Importing http module from node.js to our project
 const http = require("http");
-const PORT = 4000;
+const PORT = 5000;
 
 // Create a server 
 const server = http.createServer((request, response) => {
+    response.setHeader('Content-type', "text/html");
 
-    if (request.url === "/") {
+    if (request.url === "/dogs") {
         // Setting headers to text and html so the browser knows what
         // kind of response i will recieve
-        response.setHeader('Content-type', "text/html");
 
         // We are sending a response to the client/browser using response.write
-        response.write("<h1>Hello Thankyou for contacting me! </h1>")
+        response.write("<img src='https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_3x2.jpg'>")
+
+        // Ending the response
+        response.end();
+    }
+
+    if (request.url === "/cats") {
+        // Setting headers to text and html so the browser knows what
+        // kind of response i will recieve
+
+        // We are sending a response to the client/browser using response.write
+        response.write("<img src='https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x2.jpg'>")
 
         // Ending the response
         response.end();
