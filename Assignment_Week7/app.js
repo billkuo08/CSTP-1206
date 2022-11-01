c9b7ebfe5726ee3ee33dc8e64d256f63d431d7c7
Bill Kuo <billkuo08@gmail.com>
2022-11-01 15:33:31 -0700

    assignment week 7 ejs done

--- Assignment_Week7/app.js
+++ Assignment_Week7/app.js
@@ -2,18 +2,40 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+let today = new Date();
+
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+
+today = `${mm}/${dd}/${yyyy}`;
+let data = {
+
+    name: "Bill",
+    email: "123@gmail.com",
+    date: today,
+    contact: "1234567890",
+
+    socials: {
+
+        "https://cdn.iconscout.com/icon/free/png-128/facebook-35-83440.png": "https://www.facebook.com/",
+
+        "https://www.clipartmax.com/png/small/83-836235_ceo-and-member-of-board-of-directors-in-naghsh-avval-linkedin-icon.png": "https://www.linkedin.com/"
+
+    }
+
+}
+
 
 app.set("view engine", "ejs");
 
-let data = {
-    name: "Bill",
-    email: "123@gmail.com",
-}
+
 
 app.get("/", (req, res) => {
-   
-     //res.render("index.ejs",{name: userName, email: userEmail});
-        res.render("index.ejs", {userData: data});
+
+    //res.render("index.ejs",{name: userName, email: userEmail});
+    res.render("index.ejs", { userData: data });
+
 })
 
 
