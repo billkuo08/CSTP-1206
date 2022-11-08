0c8fceefbf4ae2a1d3699e7f353bd43d1281fbe3
Bill Kuo <billkuo08@gmail.com>
2022-11-08 12:08:40 -0800

    week 10

--- Intro_to_Mongodb/server.js
+++ Intro_to_Mongodb/server.js
@@ -3,14 +3,17 @@ const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
 require('dotenv').config();
-//User model coming from user.js
+
+// User Model coming from models
 const User = require("./models/user");
 
+// So we can accept json information
 app.use(express.json());
+
 
 mongoose.connect(process.env.MONGO_URI, (error) => {
     if (error) {
-        console.error(`There was an error : ${error}`);
+        console.error(`There was an error : ${error}` );
     } else {
         console.log("Connected to the database");
     }
@@ -21,37 +24,57 @@ app.get("/", (req, res) => {
 })
 
 app.get("/users", (req, res) => {
-    //returns async response
-    //then is waiting for the response
-    //User helps me get hold of information from mongodb atlas server
-    User.find().then((users) => {
+    // returns async response ; // This returns a promise
+    // then is waiting for the response
+    // User helps me get hold of information from mongodb atlas server
+    User.find().then((data) => {
         return res.status(200).json({
-            message: "Successfully fetched the user",
+            message: "Succesfully fetched the user",
             data
-        })
-
-    })
-
-});
-
-app.post("/users", (req, res) => {
-    let newUser = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        phoneNumber: req.body.phoneNumber
-    }
-    //We are making an instance of User Model
-    const user = new User(newUser);
-
-    user.save().then((response) => {
-        return res.status(201).json({
-            message: "User created successfully",
-            data
-        })
+        }); 
+    }).catch((error) => {
+        return res.status(404).json({
+            message: "Error",
+            error
+        }); 
     })
 })
 
+app.post("/users", async (req, res) => {
+    let newUser = {
+       name: req.body.name,
+       email: req.body.email,
+       password: req.body.password,
+       phoneNumber: req.body.phoneNumber
+    }
+
+    // We are making an instance of User Model
+    const user = new User(newUser);
+
+    // Save method helps you save data in database
+    // user.save().then((data) => {
+    //     return res.status(201).json({
+    //         message: "User succesfully created",
+    //         data
+    //     })
+    // })
+
+    try {
+
+    let response = await  user.save();
+    return res.status(201).json({
+            message: "User succesfully created",
+            response
+        })
+    } catch( error) {
+        res.status(500).json({
+            message: "Error creating user",
+            error
+        })
+    }
+
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server Running on PORT ${PORT}`);
