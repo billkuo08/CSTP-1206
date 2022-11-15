93518c9f4b09a52c7fe0b2062fc790a5abc1e0fe
Bill Kuo <billkuo08@gmail.com>
2022-11-15 09:03:44 -0800

    commit

--- midtermPart1.js
+++ midtermPart1.js
@@ -1,16 +1,58 @@
 //Q 27
 let array = [2, 4, 11, 2, 4]
-function mostRepeated(paraArr) {
-    paraArr = [2, 4, 11, 2, 4].reduce(function (acc, curr) {
-        return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
-    }, {});
-    let arr = Object.values(paraArr);
-    let min = Math.min(...arr);
-    let key = Object.keys(paraArr).find(key => paraArr[key] === min);
-    return key;
+
+function singleElement(array) {
+    let map = {};
+    let output;
+    for (let i = 0; i < array.length; i++) {
+        if (map[array[i]]) {
+            map[array[i]] = map[array[i]] + 1;
+        } else {
+            map[array[i]] = 1;
+        }
+        for (let key in map) {
+            if (map[key] === 1) {
+                output = key;
+            }
+        }
+    }
+    return output;
+
 }
 
-console.log(mostRepeated(array));
+console.log(singleElement(array));
+
+//Q 28
+let array2 = [2,4,1,2,4,2];
+
+function mostRepeated(array){
+    let map = {};
+    let max = 0;
+    let val;
+
+    for(let i = 0; i < array.length; i++){
+
+        if(map[array[i]]){
+            map[array[i]] = map[array[i]] + 1;
+        }else{
+            map[array[i]] = 1;
+        }
+    }
+
+    for(let key in map){
+        if(map[key] > max){
+            max = map[key];
+            val = key;
+        }
+    }
+    return val;
+}
+
+console.log(mostRepeated(array2)); // 2
+
+
+
+
 
 
 
