410c6842f4e166d54e9ed0656e600827f721af95
Bill Kuo <billkuo08@gmail.com>
2022-12-15 20:27:13 -0800

    sucessfully displayed the title, user and link to the issues page, need to add cards

--- GitHubAPI_Assignment/app.js
+++ GitHubAPI_Assignment/app.js
@@ -1,19 +1,18 @@
-// Get the GitHub username input form
-const gitHubForm = document.getElementById('gitHubForm');
+// Get the GitHub form
+const gitHubForm = document.getElementById('gitHub');
 
-// Listen for submissions on GitHub repo name input form
+// Listen for submissions on GitHub reponame input 
 gitHubForm.addEventListener('submit', (e) => {
 
-    // Prevent default form submission action
+    // Prevent page from refreshing
     e.preventDefault();
 
-    // Get the GitHub username input field on the DOM
-    let reponameInput = document.getElementById('usernameInput');
+    // Get the GitHub username from the input field
+    let reponameInput = document.getElementById('reponameInput');
 
-    // Get the value of the GitHub username input field
+    // Get the value of reponameInput and store in a variable called gitHubRepoName
     let gitHubRepoName = reponameInput.value;
 
-    // Run GitHub API function, passing in the GitHub username
     requestReposIssues(gitHubRepoName);
 
 });
@@ -24,17 +23,19 @@ const token = "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";
 const requestReposIssues = async (name) => {
 
     try {
-
+        //Use aync await to fetch the data from the GitHub API
         let response = await fetch(`https://api.github.com/repos/${name}/issues`, {
             headers: {
                 'Accept': 'application/vnd.github.v3+json',
-                'Authorization': `token ${token}`
+                //Kept running into status 401 error so I commented it out
+                //'Authorization': `token ${token}`
             }
 
         });
 
         let output = await response.json();
         console.log(output);
+        //Call teh formatResults function and pass in the output to display the results on the html page
         formatResults(output);
 
     } catch (error) {
@@ -43,26 +44,26 @@ const requestReposIssues = async (name) => {
 }
 
 const formatResults = (data) => {
-    // Loop over each object in data array
+    // Use a for loop to iterate through the data
     for (let i in data) {
 
-        // Get the ul with id of of issuesRepo
-        let ul = document.getElementById('issuesRepo');
+        //Locate the unordered list with its id
+        let ul = document.getElementById('userRepos');
 
-        // Create variable that will create li's to be added to ul
+        //Create a list
         let li = document.createElement('li');
 
-        // Add Bootstrap list item class to each li
+        //Add a class to the list
         li.classList.add('list-group-item')
 
-        // Create the html markup for each li
+        //console.log(data[i].title);
+        // Add the title, User and URL datas to the list
         li.innerHTML = (`
             <p><strong>Title:</strong> ${data[i].title}</p>
             <p><strong>User:</strong> ${data[i].user.login}</p>
             <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
         `);
-
-        // Append each li to the ul
+        //Append the list to the unordered list(bullet list)
         ul.appendChild(li);
 
     }
