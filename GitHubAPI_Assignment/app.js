fa6b5bb6902bd67b9fa85ec7f6c6087787ac2d36
Bill Kuo <billkuo08@gmail.com>
2022-12-15 18:55:19 -0800

    trying async function this time, returning 'Bad credentials' error

--- GitHubAPI_Assignment/app.js
+++ GitHubAPI_Assignment/app.js
@@ -3,7 +3,7 @@ const gitHubForm = document.getElementById('gitHubForm');
 
 // Listen for submissions on GitHub repo name input form
 gitHubForm.addEventListener('submit', (e) => {
-    
+
     // Prevent default form submission action
     e.preventDefault();
 
@@ -11,7 +11,7 @@ gitHubForm.addEventListener('submit', (e) => {
     let reponameInput = document.getElementById('usernameInput');
 
     // Get the value of the GitHub username input field
-    let gitHubRepoName = reponameInput.value;          
+    let gitHubRepoName = reponameInput.value;
 
     // Run GitHub API function, passing in the GitHub username
     requestReposIssues(gitHubRepoName);
@@ -20,20 +20,26 @@ gitHubForm.addEventListener('submit', (e) => {
 
 const token = "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";
 
-const requestReposIssues = (name) => {
-    fetch(`https://api.github.com/repos/${name}/issues`,
-        {
+
+const requestReposIssues = async (name) => {
+
+    try {
+
+        let response = await fetch(`https://api.github.com/repos/${name}/issues`, {
             headers: {
                 'Accept': 'application/vnd.github.v3+json',
                 'Authorization': `token ${token}`
             }
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            formatResults(data);
-        })
-        .catch(error => console.error(error))
+
+        });
+
+        let output = await response.json();
+        console.log(output);
+        formatResults(output);
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const formatResults = (data) => {
@@ -45,19 +51,19 @@ const formatResults = (data) => {
 
         // Create variable that will create li's to be added to ul
         let li = document.createElement('li');
-        
+
         // Add Bootstrap list item class to each li
         li.classList.add('list-group-item')
-    
+
         // Create the html markup for each li
         li.innerHTML = (`
             <p><strong>Title:</strong> ${data[i].title}</p>
             <p><strong>User:</strong> ${data[i].user.login}</p>
             <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
         `);
-        
+
         // Append each li to the ul
         ul.appendChild(li);
-    
+
     }
 }
